230f353b3834d267cb5668eefeb7a3c8dc755591
Vladimir V <vladimir.vorobiev@gmail.com>
2024-01-28 14:24:49 -0500

    stop mongo

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+// import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
 import { UserModule } from '../user/user.module';
@@ -7,11 +7,11 @@ import { BookmarkModule } from '../bookmark/bookmark.module';
 
 @Module({
   imports: [
-    MongooseModule.forRootAsync({
-      useFactory: () => ({
-        uri: process.env.DATABASE_URL,
-      }),
-    }),
+    // MongooseModule.forRootAsync({
+    //   useFactory: () => ({
+    //     uri: process.env.DATABASE_URL,
+    //   }),
+    // }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
